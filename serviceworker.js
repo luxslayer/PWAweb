491957e11a1990cb5db1d651fad9325da9dad97a
luxslayer <luxslayer@users.noreply.github.com>
2026-02-28 00:51:54 +0000

    deploy: 1e0041fbadf8d5ac9723e93e7aaec02de8dca035

--- serviceworker.js
+++ serviceworker.js
@@ -5,8 +5,8 @@ const FILES_TO_CACHE = [
   '/style.css',
   '/main.js',
   '/manifest.json',
-  '/icons/ABpoint192.png',
-  '/icons/ABpoint512.png'
+  '/icons/ABpoint192.ico',
+  '/icons/ABpoint512.ico'
 ];
 
 // Instalación
